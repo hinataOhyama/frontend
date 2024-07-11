852d0cb8760201eaba380993c4d8306db15fd78a
hinata.o <hinata.o@infratop.jp>
2024-07-11 23:27:42 +0900

    setup prettier and eslint, add dir tree

---
+++ .prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+export default {
+  plugins: ["prettier-plugin-organize-imports"],
+  semi: false,
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  trailingComma: 'all',
+  jsxSingleQuote: true,
+}
